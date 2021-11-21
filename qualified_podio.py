8f486c78c81e03cf589b812fb479160ada845678
Gabriela Antunes <gabriela.lantunes@gmail.com>
2021-11-20 21:29:56 -0300

    Execício de Git - questão 3 do qualified 4

---
+++ qualified_podio.py
@@ -0,0 +1,8 @@
+def podio_olimpico(t1, t2, t3):
+  tempos = [t1, t2, t3]
+  tempos_ordenados = (sorted(tempos))
+  return (
+    f"1 - {tempos_ordenados[0]} minutos\n"
+    f"2 - {tempos_ordenados[1]} minutos\n"
+    f"3 - {tempos_ordenados[2]} minutos\n"
+  )
